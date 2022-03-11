45cfa4b698100d6264cb8cfec2db1ff264647a5d
Eva <rutheve.eva@gmail.com>
2022-03-11 20:56:45 +0300

    center text and bolden header

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,8 +1,16 @@
+
+
+
+//styling the what we do section. Center aligned and bold header
+$('.info').css("textAlign","center");
+$('info-h4').css("font-weight","bold")
+
+
+
+
+// Adding backgrounds to the portfolio section
 let portfolioBackgrounds = ["work1.jpg","work2.jpg","work3.jpg","work4.jpg","work5.jpg","work6.jpg","work7.jpg","work8.jpg"];
 let portfolioClasses = ["one","two","three","four","five","six","seven","eight"];
-console.log(portfolioClasses[2]);
-var yoh = portfolioClasses[2];
-
 const addBackgrounds = ()=>{
   for(var count=0; count<portfolioBackgrounds.length; count++){
   
@@ -12,5 +20,5 @@ const addBackgrounds = ()=>{
 
 }
 addBackgrounds();
-
+//End of addition of backgrounds
 

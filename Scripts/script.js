23546b7930ab9ac1b3960633c6f062c7312a8630
Eva <rutheve.eva@gmail.com>
2022-03-12 01:36:01 +0300

    feature: add more functionality

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,7 +3,7 @@
 
 //styling the what we do section. Center aligned and bold header
 $('.info').css("textAlign","center");
-$('info-h4').css("font-weight","bold")
+$('.info-h4').css("font-weight","bold")
 
 // Adding backgrounds to the portfolio section
 let portfolioBackgrounds = ["work1.jpg","work2.jpg","work3.jpg","work4.jpg","work5.jpg","work6.jpg","work7.jpg","work8.jpg"];
